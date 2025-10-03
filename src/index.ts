589c9ce963a91933caee894655252d6d621e9310
raddva <111165508+raddva@users.noreply.github.com>
2025-10-03 22:35:25 +0700

    feat: Add Global Error Handler

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import router from "./routes/api";
 import db from "./utils/database";
 import docs from "./docs/route";
+import errorMiddleware from "./middlewares/error.middleware";
 
 async function init() {
   try {
@@ -25,6 +26,9 @@ async function init() {
     app.use("/api", router);
 
     docs(app);
+
+    app.use(errorMiddleware.serverRoute());
+    app.use(errorMiddleware.serverError());
 
     app.listen(PORT, () =>
       console.log(`Server is running on http://localhost:${PORT}`)
